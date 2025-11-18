c82ce3d02621dffe5a4d2b26e40dfcde5ee426c9
balaji-githubstore <balajidinakaran1@gmail.com>
2025-11-18 12:16:12 +0530

    Update on assignment

---
+++ src/demo6-practice.ts
@@ -0,0 +1,7 @@
+// work on type alias
+// work on method creation (assignment 1)
+// work on named method creation
+
+//work on class - static vs non-static method variable (assignment 2 will do)
+
+//try installing some apk in emulator with code available in demo5-install-app.ts

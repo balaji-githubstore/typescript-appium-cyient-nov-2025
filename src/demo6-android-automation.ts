0c46f750902bff040c00aea85b8380d3e37b23aa
balaji-githubstore <balajidinakaran1@gmail.com>
2025-11-19 09:34:25 +0530

    gitignore

--- src/demo6-android-automation.ts
+++ src/demo6-android-automation.ts
@@ -1,17 +1,17 @@
-import {remote} from "webdriverio"
+import { remote } from "webdriverio"
 
 async function invalidLoginTest() {
 
-    const driver= await remote({
-        "protocol":"http",
-        "hostname":"127.0.0.1",
-        "port":4723,
-        "capabilities":{
-            "platformName":"Android",
-            "appium:automationName":"uiautomator2",
-            "appium:deviceName":"oneplus",
-            "appium:app":"D:\\Balaji\\Company\\Cyient Nov 2025\\TypescriptAutomationProject\\files\\khan-academy-7-3-2.apk",
-            "appium:udid":"f1d22477"
+    const driver = await remote({
+        "protocol": "http",
+        "hostname": "127.0.0.1",
+        "port": 4723,
+        "capabilities": {
+            "platformName": "Android",
+            "appium:automationName": "uiautomator2",
+            "appium:deviceName": "oneplus",
+            "appium:app": "D:\\Balaji\\Company\\Cyient Nov 2025\\TypescriptAutomationProject\\files\\khan-academy-7-3-2.apk",
+            "appium:udid": "f1d22477"
         }
     })
 
@@ -25,7 +25,7 @@ async function invalidLoginTest() {
     await driver.$("//android.widget.EditText[contains(@text,'e-mail address')]").setValue("hello@gmail.com")
 
     await driver.pause(3000)
-    
+
 }
 
 
